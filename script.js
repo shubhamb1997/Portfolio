3a3acc2fae5e0667b29900077a41646e4ebb0bf9
shubhamb1997 <bhattsb27@gmail.com>
2022-06-28 14:03:36 +0530

    updated

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["YouTuber", "Developer", "Gamer", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
